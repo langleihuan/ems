1a02e5cbfc9d1899cdac84b90fffbcb71fc880a7
zwm <1280295064@qq.com>
2019-12-06 19:39:34 +0800

    1

--- ems/src/main/java/com/baizhi/entity/Emp.java
+++ ems/src/main/java/com/baizhi/entity/Emp.java
@@ -1,10 +1,12 @@
 package com.baizhi.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Date;
 
@@ -13,9 +15,11 @@ import java.util.Date;
 @NoArgsConstructor
 @Accessors(chain = true)
 public class Emp implements Serializable {
+    @Id
     private String id;
     private String name;
     private Double salary;
     private Integer age;
+    @JsonFormat(pattern = "yyyy/MM/dd")
     private Date create_time;
 }
